de9be21f002ae43991772988eee14ce39cebaa2b
FengXingyuan <landswimmer@hotmail.com>
2020-11-19 10:08:38 +0800

    feat(tabs): finish tabs

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <el-row type="flex" justify="center">
-    <el-col :span="18">
-      <Header />
-    </el-col>
-  </el-row>
+  <div>
+    <el-row type="flex" justify="center">
+      <el-col :span="18">
+        <Header />
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="center">
+      <el-col :span="18">
+        <Tabs :tabs="headerTabs" name="headerTabs" />
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
 import Header from "./components/Header.vue";
+import Tabs from "./components/Tabs.vue";
 import { Row, Col } from "element-ui";
 import "./normalize.css";
 
@@ -18,7 +26,23 @@ Vue.component(Col.name, Col);
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Tabs
+  },
+  data: function() {
+    return {
+      headerTabs: [
+        "首页",
+        "歌手",
+        "新碟",
+        "排行榜",
+        "分类歌单",
+        "电台",
+        "MV",
+        "数字专辑",
+        "票务"
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tabs">
+    <input type="radio" :name="name" id="-1" checked />
+    <label class="tabs__item" for="-1">{{ tabs[0] }}</label>
+    <template v-for="(item, index) in list">
+      <input type="radio" :name="name" :id="index" :key="index" />
+      <label class="tabs__item" :for="index" :key="index">{{ item }}</label>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+    tabs: Array,
+    name: String
+  },
+  computed: {
+    list: function() {
+      return this.tabs.slice(1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.tabs {
+  width: 100%;
+  height: 51px;
+  line-height: 51px;
+  display: flex;
+  justify-content: center;
+  input[type="radio"] {
+    display: none;
+  }
+  &__item {
+    margin: 0 24px;
+    font-size: 15px;
+    &:hover {
+      color: #31c27c;
+    }
+  }
+}
+
+input[type="radio"]:checked + label {
+  color: #31c27c;
+}
+</style>
